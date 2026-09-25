1793d6de6aba007391c8c48b16ab509c5c4384c5
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-25 12:41:39 +0200

    Refactor header links for cleaner markup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,10 @@ function Header() {
   return (
     <header className="header">
       <nav className="header-nav">
-        <a href="/">
-          Home
-        </a>
-        <a href="/products">
-          Products
-        </a>
-        <a href="/cart">
-          Cart
-        </a>
-        <a href="/login">
-          Login
-        </a>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/cart">Cart</a>
+        <a href="/login">Login</a>
       </nav>
     </header>
   );
