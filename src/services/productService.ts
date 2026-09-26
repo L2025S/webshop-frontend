9332fb090c2fab073ffed2a2fd85e40f04f334fe
L.W. <reaktor.utfallen-2z@icloud.com>
2026-09-26 15:01:20 +0200

    Add productService

---
+++ src/services/productService.ts
@@ -0,0 +1,43 @@
+import type { Product } from "../types/Product";
+import { ApiError } from "../errors/ApiError";
+
+const PRODUCT_SERVICE_URL= import.meta.env.VITE_PRODUCT_SERVICE_URL;
+
+export async function fetchAllProducts(): Promise<Product[]> {
+    const token = sessionStorage.getItem("accessToken");
+
+    if(!token) {
+        throw new ApiError ("You are not logged in. Please log in to view products.");
+    }
+
+    let response: Response;
+
+    try {
+        response = await fetch (`${PRODUCT_SERVICE_URL}/products/all`, {
+            method:"GET",
+            headers: {
+                Authorization:`Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        });
+
+    } catch (networkError) {
+        console.error(networkError);
+        throw new ApiError ("Could not connect to the server. Please check your network connection.");
+
+    }
+
+    if (!response.ok){
+        if(response.status === 401 || response.status === 403) {
+            throw new ApiError ("Your session has expired or you don't have permission. Please log in again.", response.status);
+        }
+
+        throw new ApiError ( 
+            `Failed to load products (status code: ${response.status})`,
+            response.status
+        );
+    }
+
+    const data: Product[] = await response.json();
+    return data;
+};
